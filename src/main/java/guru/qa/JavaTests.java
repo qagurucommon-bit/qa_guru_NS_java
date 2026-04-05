4a7398c81e43e680303ef7547f4daeed5779bdb6
Nelli Sabitova <eillen@mail.ru>
2026-04-05 12:02:48 +0300

    New repository Java tests

---
+++ src/main/java/guru/qa/JavaTests.java
@@ -0,0 +1,13 @@
+package guru.qa;
+
+
+public class JavaTests {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+           System.out.println("i = " + i);
+        }
+    }
+}
