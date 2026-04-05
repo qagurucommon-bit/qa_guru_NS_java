689c8341ecaa063e5e1e3ca8b1c1d65faa54816c
Nelli Sabitova <eillen@mail.ru>
2026-04-05 12:38:13 +0300

    JavaTests finished

--- src/main/java/guru/qa/JavaTests.java
+++ src/main/java/guru/qa/JavaTests.java
@@ -1,13 +1,31 @@
 package guru.qa;
 
-
 public class JavaTests {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        // 0) применить несколько арифметических операций ( + , -, * , /) над двумя примитивами типа int
+        int a = 9;
+        int b = 5;
+        System.out.println("a+b= " + (a+b));
+        System.out.println("a-b= " + (a-b));
+        System.out.println("a*b= " + (a*b));
+        System.out.println("b/a= " + (a/b));
 
-        for (int i = 1; i <= 5; i++) {
-           System.out.println("i = " + i);
-        }
+        // 1) применить несколько арифметических операций над int и double в одном выражении
+        double c = 2.2;
+        System.out.println("(a+b-c*c/a)= " + (a+b-c*c/a));
+
+        // 2) применить несколько логических операций ( < , >, >=, <= )
+        System.out.println("c<b= " + (c<b));
+        System.out.println("a>b= " + (a>b));
+        System.out.println("a>=c= " + (a>=c));
+        System.out.println("a<=b= " + (a<=b));
+
+        // 4) получить переполнение при арифметической операции
+
+        int maxValue = Integer.MAX_VALUE;
+        int maxResult = Integer.MAX_VALUE + 1;
+        System.out.println("maxValue + 1 = " + (maxResult));
+
     }
-}
+    }
